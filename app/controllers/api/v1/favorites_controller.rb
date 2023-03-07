be134061dde2b872041ab945c650725003283205
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-03-07 12:25:13 -0600

    creates user favorites endpoint

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -8,4 +8,9 @@ class Api::V1::FavoritesController < ApplicationController
       raise CreateError.new(details: favorite.errors.full_messages.to_sentence), status: :bad_request
     end
   end
+
+  def index
+    user_favorites = User.find_by(api_key: params[:api_key]).favorites
+    render json: UserFavoritesSerializer.favorites(user_favorites)
+  end
 end
